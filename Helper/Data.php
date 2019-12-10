6c6d408af30a11e6c51da7a4a9fd157c77fd962e
Oleg K <voknevark@gmail.com>
2019-12-09 16:25:31 -0800

    Update Data.php

--- Helper/Data.php
+++ Helper/Data.php
@@ -7,13 +7,6 @@ use Magento\Framework\App\Helper\Context;
 
 class Data extends DataHelper
 {
-    
-    public function __construct(
-        \Magento\Framework\App\Helper\Context $context
-    ) {
-        parent::__construct($context);
-    }
-    
     public function isEnabled()
     {
         return $this->getConfigFlag('hapex_logcleanup/general/enable');
