9d64b53a8088401cbf354adfd563422bd6fc16cf
Oleg K <voknevark@gmail.com>
2020-02-07 10:11:48 -0800

    Update Cron/Cleanup.php

--- Cron/Cleanup.php
+++ Cron/Cleanup.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Hapex\LogCleanup\Cron;
 
-use Hapex\LogCleanup\Helper\Data as DataHelper; 
+use Hapex\LogCleanup\Helper\Data as DataHelper;
 use Magento\Framework\App\ResourceConnection;
 use Psr\Log\LoggerInterface;
 
@@ -17,14 +17,11 @@ class Cleanup
      * @var LoggerInterface
      */
     protected $logger;
-    
+
     private $helperData;
 
-    public function __construct(
-        DataHelper $helperData,
-        ResourceConnection $resource,
-        LoggerInterface $logger
-    ) {
+    public function __construct(DataHelper $helperData, ResourceConnection $resource, LoggerInterface $logger)
+    {
         $this->helperData = $helperData;
         $this->resource = $resource;
         $this->logger = $logger;
@@ -32,8 +29,7 @@ class Cleanup
 
     public function cleanLogs()
     {
-        switch($this->helperData->isEnabled())
-        {
+        switch ($this->helperData->isEnabled()) {
             case true:
                 $this->helperData->log("");
                 $this->helperData->log("--- Starting Log File Cleanup ---");
@@ -44,31 +40,26 @@ class Cleanup
                     $this->helperData->log("---- Getting log files list ----");
                     $files = $this->helperData->getLogFiles();
                     $this->helperData->log("---- Looking for any log file larger than $maxSize MB in size ----");
-                    foreach ($files as $file)
-                    {
+                    foreach ($files as $file) {
                         $size = $this->helperData->getFileSize($file) / 1024 / 1024;
-                        switch($size >= $maxSize)
-                        {
+                        switch ($size >= $maxSize) {
                             case true:
                                 $this->helperData->deleteFile($file);
                                 $counter++;
                                 $this->helperData->log("---- Deleted $file of size $size MB ----");
                                 break;
-                        }                
+                        }
                     }
-                    
+
                     $message = "---- " . ($counter == 0 ? "No" : $counter) . " overgrown log files found and deleted ----";
                     $this->helperData->log($message);
-                    
                 } catch (\Exception $e) {
                     $this->helperData->log(sprintf('Error: %s', $e->getMessage()));
-                }
-                finally
-                {
+                } finally {
                     $this->helperData->log("--- Ending Log File Cleanup ---");
+                    return $this;
                 }
-        
-                return $this;
+                break;
         }
     }
 }
