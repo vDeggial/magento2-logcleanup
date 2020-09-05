3ecd998f1ec3a4d6509aa85595a6ad9835fe70c6
Oleg K <voknevark@gmail.com>
2020-09-05 09:19:36 -0700

    Update Helper\Data.php

--- app/code/Hapex/LogCleanup/Helper/Data.php
+++ app/code/Hapex/LogCleanup/Helper/Data.php
@@ -3,19 +3,12 @@
 namespace Hapex\LogCleanup\Helper;
 
 use Hapex\Core\Helper\DataHelper;
-use Magento\Framework\App\Helper\Context;
-use Magento\Framework\ObjectManagerInterface;
 
 class Data extends DataHelper
 {
     protected const XML_PATH_CONFIG_ENABLED = "hapex_logcleanup/general/enable";
     protected const XML_PATH_CONFIG_MAXSIZE = "hapex_logcleanup/general/max_size";
     protected const FILE_PATH_LOG = "hapex_log_cleanup";
-    public function __construct(Context $context, ObjectManagerInterface $objectManager)
-    {
-
-        parent::__construct($context, $objectManager);
-    }
 
     public function isEnabled()
     {
